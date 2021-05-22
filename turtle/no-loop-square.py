8d5d8d1c21a28e688a2c64be199cfaa5947558c9
Hussein Elguindi <34726859+HusseinElguindi@users.noreply.github.com>
2021-05-22 13:31:24 -0400

    turtle: no loop square

---
+++ turtle/no-loop-square.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Pen()
+
+# Top side
+t.forward(100) # Go forward 100
+t.right(90) # Turn 90 deg right
+
+# Right side
+t.forward(100)
+t.right(90)
+
+# Bottom side
+t.forward(100)
+t.right(90)
+
+# Left side
+t.forward(100)
+t.right(90)
